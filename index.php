f4231b7f635f9b512a4e5462b0c3d51e03fada01
kelasberat88 <ysetiawanj@atmaluhur.ac.id>
2021-04-12 08:50:58 +0700

    jquery

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php
-?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<select id="selectsample" name="samp">
+    <option value="1" grouping="Yes"  >first</option>
+    <option value="2" grouping="No"  >four</option>
+</select>
+<input type='text' id='txt1'>
+<div id="Divid"></div>
+<script>
+  function ShowHideDiv()
+  {
+      var groupattr = $("option:selected", "#selectsample").attr("grouping");
+      if (groupattr == "Yes")
+          {
+              $("#Divid").show();
+          }
+      else
+          {
+              $("#Divid").hide();
+          }
+  }
+
+  $(document).ready(function () {
+      //Let's run this once in case "Yes" is the default (first) option in the dropdown
+          ShowHideDiv();
+      $("#selectsample").on("change", function () {
+          ShowHideDiv();
+      });
+  });
+
+</script>
